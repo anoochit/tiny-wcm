382e7fd4a4f1e2044f04557f5e67efb3c26bda75
anoochit <anoochit@87d7af5c-0221-11de-99c0-75d572ba1ddd>
2009-04-17 05:59:57 +0000

    mocup system

---
+++ trunk/config.inc.php
@@ -0,0 +1,54 @@
+<?php
+/*
+Copyright (C) 2008 Anoochit Chalothorn <anoochit@redlinesoft.net>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/**
+ * Database Setting
+ */
+$cfg_db="tinywcm";
+$cfg_host="localhost";
+$cfg_username="raziel";
+$cfg_password="monalisa";
+
+/**
+ *  Misc Initial Config
+ */
+$cfg_url="http://tinywcm.redlinesoft.net";
+$cfg_subdir="";
+$cfg_dir="D:\\redlinesoft\\www\\tinywcm\\trunk";
+$cfg_lang="english";
+$cfg_theme="default";
+$cfg_offline="n";
+
+
+/**
+ * SMTP
+ */
+$cfg_smtp_host="localhost";
+$cfg_smtp_port="25";
+
+/**
+ * Email administration
+ */
+
+$cfg_admin_name="support";
+$cfg_admin_email="support@brainstorm.redlinesoft.net";
+
+
+
+?>
